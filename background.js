c3d6f4dd0a802bfd0c4102b0c12f8b8cece0e2d0
Dean Sofer <dsofer@paxata.com>
2013-05-02 15:44:41 -0700

    Added action suggestions for a repo

--- background.js
+++ background.js
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 var base = 'https://github.com/';
+var travisBase = 'https://travis-ci.org/';
 
 var tools = {
   http: function(url) {
@@ -23,16 +24,19 @@ var tools = {
     });
   },
   user: function(text) {
-    return text.match(/@(\w+)/);
+    return text.match(/@([\w-]+)/);
   },
   repo: function(text) {
-    return text.match(/(\w+\/\w+)(\W+.+)?/);
+    return text.match(/([\w-]+\/[\w-]+)(\s+.+)?/);
   },
   repoAction: function(text) {
-    return text.match(/(pulse|wiki|pulls|graphs|network|issues|admin|new pull|new issue|#([0-9]+)|@(\w+))/);
+    return text.match(/(pulse|wiki|pulls|graphs|network|issues|admin|travis|new pull|new issue|#([0-9]+)|@([\w-]+))/);
+  },
+  isUrl: function(text) {
+    return text.match(/http[s]?:.*/);
   },
   io: function(text) {
-    return text.match(/(\w+)\.(gh|github)\.(io|com)(\/\w+)?/);
+    return text.match(/([\w-]+)\.(gh|github)\.(io|com)(\/[\w-]+)?/);
   },
   ioUrl: function(io) {
     var url;
@@ -46,36 +50,37 @@ var tools = {
   }
 };
 
-function setDefault(text) {
-  var type, action, content = 'Go to ',
-    description = '';
+function setDefault(text, suggest) {
+  var type, action, description = 'Go to ';
 
-  if (type = tools.user(text)) {
-    content += 'User:';
-    description = base + type[1];
+  if (type = tools.isUrl(text)) {
+    description = 'Go to';
+  } else if (type = tools.user(text)) {
+    description += 'user';
   } else if (type = tools.repo(text)) {
+    suggestActions(text, suggest);
     if (type[2] && (action = tools.repoAction(type[2]))) {
       if (action[2]) {
-        content += 'Repo Issue:';
+        description += 'repo issue';
       } else if (action[3]) {
-        content += 'Repo Branch:';
+        description += 'repo branch';
       } else {
-        content += 'Repo '+action[1]+':';
+        if (action[1] === 'travis')
+          description += action[1];
+        else
+          description += 'repo '+action[1];
       }
     } else {
-      content += 'Repo:';
-      description = base + type[1];
+      description += 'repo';
     }
   } else if (type = tools.io(text)) {
-    content += 'Page:';
-    description = tools.ioUrl(type);
+    description += 'page';
   } else {
-    content = 'Search for:';
-    description = tools.searchUrl(text);
+    description = 'Search for';
   }
 
   chrome.omnibox.setDefaultSuggestion({
-    description: content + ' %s'
+    description: description + ': %s'
   });
 }
 
@@ -84,6 +89,22 @@ function setSuggestions(text, suggest) {
     {content: text + " one", description: "the first one"},
     {content: text + " number two", description: "the second entry"}
   ]);
+}
+
+function suggestActions(text, suggest) {
+  var suggestions = [],
+    description = 'Go to ',
+    content = base + text + '/',
+    actions = ['pulse','wiki','pulls','graphs','network','issues','admin'];
+
+  for (var i = actions.length - 1; i >= 0; i--) {
+    suggestions.push({ description: description + actions[i] + ': ' + text, content: content + actions[i] });
+  };
+  suggestions.push({ description: 'New pull: ' + text, content: content + 'issues/new' });
+  suggestions.push({ description: 'New issue: ' + text, content: content + 'pulls/new' });
+  suggestions.push({ description: 'Go to travis: ' + text, content: travisBase + text });
+
+  suggest(suggestions);
 }
 
 var global = {
@@ -111,7 +132,7 @@ var global = {
 // as long as the extension's keyword mode is still active.
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
-    setDefault(text);
+    setDefault(text, suggest);
 
     // setSuggestions(text, suggest);
   });
@@ -119,16 +140,22 @@ chrome.omnibox.onInputChanged.addListener(
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(function(text) {
   var type, action;
-  if (type = tools.user(text)) {
+  if (type = tools.isUrl(text)) {
+    tools.navigate(text);
+  } else if (type = tools.user(text)) {
     tools.navigate(type[1]);
   } else if (type = tools.repo(text)) {
+    console.log(type);
     if (type[2] && (action = tools.repoAction(type[2]))) {
       if (action[2]) {
         tools.navigate(type[1] + '/issues/' + action[2]);
       } else if (action[3]) {
         tools.navigate(type[1] + '/tree/' + action[3]);
       } else {
-        tools.navigate(type[1] + '/' + action[1]);
+        if (action[1] === 'travis')
+          tools.navigate(travisBase + type[1], true);
+        else
+          tools.navigate(type[1] + '/' + action[1]);
       }
     } else {
       tools.navigate(type[1]);
