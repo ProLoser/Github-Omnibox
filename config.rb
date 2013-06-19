f80df23eea52c00d94c3d96a820897cac28307f0
Dean Sofer <proloser@hotmail.com>
2013-06-19 02:04:38 -0700

    Reduced unneeded clutter

--- config.rb
+++ config.rb
@@ -6,8 +6,6 @@
 http_path = ""
 css_dir = ""
 sass_dir = ""
-images_dir = "../img"
-javascripts_dir = "../js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
