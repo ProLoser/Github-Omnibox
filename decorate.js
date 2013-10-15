aa56bde31b55f2fda3a9744ee365faab535802f0
Dean Sofer <proloser@hotmail.com>
2013-10-15 16:30:44 -0700

    Adding more Dependency check images

--- decorate.js
+++ decorate.js
@@ -1,25 +1,27 @@
 var menu = '<li class="github-omnibox-sidebar-item tooltipped leftwards" title="Travis CI"> \
-        <a href="http://travis-ci.org/{owner}/{repo}"> \
-            <span class="octicon github-omnibox-icon-travis"><img src="http://travis-ci.org/favicon.ico"></span> \
-            <span class="full-word"><img src="https://secure.travis-ci.org/{owner}/{repo}.png"></span> \
+        <a href="https://travis-ci.org/{owner}/{repo}"> \
+            <span class="octicon"><img src="https://travis-ci.org/favicon.ico"></span> \
+            <span class="full-word"><img src="https://secure.travis-ci.org/{owner}/{repo}.png" alt="Travis CI" onerror="$(this).after(\'Travis CI Unavailable\')"></span> \
         </a> \
     </li> \
     <li class="github-omnibox-sidebar-item tooltipped leftwards" title="Gemnasium"> \
         <a href="https://gemnasium.com/{owner}/{repo}"> \
-            <span class="octicon github-omnibox-icon-david"><img src="https://gemnasium.com/favicon.png"></span> \
-            <span class="full-word"><img src="https://gemnasium.com/{owner}/{repo}.png"></span> \
-        </a> \
-    </li>';
-
-/*
-Coveralls:
-    <li class="github-omnibox-sidebar-item tooltipped leftwards" title="Coveralls"> \
-        <a href="https://coveralls.io/r/{owner}/{repo}"> \
-            <span class="octicon github-omnibox-icon-david"><img src="http://coveralls.io/favicon.ico"></span> \
-            <span class="full-word"><img src="https://coveralls.io/repos/{owner}/{repo}/badge.png"></span> \
+            <span class="octicon"><img src="https://gemnasium.com/favicon.png"></span> \
+            <span class="full-word"><img src="https://gemnasium.com/{owner}/{repo}.png" alt="Gemnasium" onerror="$(this).after(\'Gemnasium Unavailable\')"></span> \
         </a> \
     </li> \
-*/ 
+    <li class="github-omnibox-sidebar-item tooltipped leftwards" title="David DM"> \
+        <a href="https://david-dm.org/{owner}/{repo}"> \
+            <span class="octicon"><img src="https://david-dm.org/favicon.ico"></span> \
+            <span class="full-word"><img src="https://david-dm.org/{owner}/{repo}.png" alt="David DM" onerror="$(this).after(\'David DM Unavailable\')"></span> \
+        </a> \
+    </li> \
+    <li class="github-omnibox-sidebar-item tooltipped leftwards" title="Coveralls"> \
+        <a href="https://coveralls.io/r/{owner}/{repo}"> \
+            <span class="octicon"><img src="https://coveralls.io/favicon.ico"></span> \
+            <span class="full-word"><img src="https://coveralls.io/repos/{owner}/{repo}/badge.png" alt="Coveralls" onerror="$(this).after(\'Coveralls Unavailable\')"></span> \
+        </a> \
+    </li>';
 
 // The navigation sidebar
 var target = document.querySelector('.repo-nav-contents');
